ba85947344aed80c96514ea6481012f3b0b7160a
erik savage <erik.k.savage@gmail.com>
2024-12-01 12:25:11 -0800

    2024 day1 part 2

--- 2024/day1/historianHysteria.js
+++ 2024/day1/historianHysteria.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
-const filepath = "./2024/day1/testInput.txt";
-// const filepath = "./2024/day1/input.txt";
+// const filepath = "./2024/day1/testInput.txt";
+const filepath = "./2024/day1/input.txt";
 
 async function getData() {
   const data = await fs.promises.readFile(filepath, 'utf8');
@@ -24,6 +24,24 @@ function findDistance(list1, list2) {
   }, 0);
 }
 
+function createListNumHash(list) {
+  const numHash = {};
+
+  list.forEach(num => {
+    let count = numHash[num];
+    count === undefined ? numHash[num] = 1 : numHash[num] = count + 1;
+  });
+
+  return numHash;
+}
+
+function generateSimularityScore(list, numHash) {
+  return list.reduce((acc, cur) => {
+    score = numHash[cur] === undefined ? 0 : cur * numHash[cur];
+    return acc + score;
+  }, 0);
+}
+
 async function generatePart1Answer() {
   const dataArray = await getData();
   let list1 = [];
@@ -40,8 +58,10 @@ async function generatePart2Answer() {
   let list2 = [];
   fillLists(dataArray, list1, list2);
 
-  const totalDistance = findDistance(list1.sort(), list2.sort());
-  console.log(totalDistance);
+  const list2NumHash = createListNumHash(list2);
+  const simularityScore = generateSimularityScore(list1, list2NumHash);
+  console.log(simularityScore);
+
 }
 
 generatePart1Answer();
